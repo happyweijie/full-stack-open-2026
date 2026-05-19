23610f137a9169a06fc8cfe07e28ba6883fb5a8d
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-05-20 00:31:58 +0800

    Remove dummy contacts data

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,9 +6,6 @@ import SearchBar from './components/SearchBar'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ])
   
   // add a new person to the phonebook if name doesn't already exist
