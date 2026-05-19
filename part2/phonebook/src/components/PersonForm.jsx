411f083644b19efceb26a701ce4713e75cd9d1ff
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-05-19 23:34:59 +0800

    Add exercise 2.8

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -5,6 +5,10 @@ const PersonForm = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState('')
   const handleNameChange = (event) => setNewName(event.target.value)
 
+  // new name to be added to the phonebook
+  const [newPhone, setNewPhone] = useState('')
+  const handlePhoneChange = (event) => setNewPhone(event.target.value)
+
   // add a new name to the phonebook if it doesn't already exist
   const addPerson = (persons, setPersons) => (event) => {
     event.preventDefault()
@@ -16,7 +20,9 @@ const PersonForm = ({ persons, setPersons }) => {
     }
 
     setPersons(persons.concat({
-      name: newName
+      name: newName,
+      phone: newPhone,
+      id: persons.length + 1
     }))
     setNewName('')
   }
@@ -24,7 +30,10 @@ const PersonForm = ({ persons, setPersons }) => {
   return (
     <form onSubmit={addPerson(persons, setPersons)} >
       <div>
-        name: <input value={newName} onChange={handleNameChange} />
+        name: <input value={newName} onChange={handleNameChange} required/>
+      </div>
+      <div>
+        phone: <input value={newPhone} onChange={handlePhoneChange} required/>
       </div>
       <div>
         <button type="submit">add</button>
--- part2/phonebook/src/components/PersonList.jsx
+++ part2/phonebook/src/components/PersonList.jsx
@@ -3,7 +3,7 @@ const PersonList = ({ persons }) => {
     <ul>
       {persons.length === 0
         ? <li>No contacts.</li>
-        : persons.map((p) => <li key={p.name}>{p.name}</li>)
+        : persons.map((p) => <li key={p.id}>{p.name} {p.phone}</li>)
       }
     </ul>
   )
