12bac473ce7caa994312617b93486572a68b6997
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-02 21:59:52 +0800

    Add 3.11 serve frontend build from backend

--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 morgan.token('body', (req, res) => JSON.stringify(req['body']));
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
