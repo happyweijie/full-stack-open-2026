db28b51d943c744d98c647f16e689df813d7a5a9
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-06 12:13:34 +0800

    Add cli script for communicating with MongoDB

---
+++ part3/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const process = require('node:process');
+require('dotenv').config();
+
+if (process.argv.length < 3) {
+  console.log('Format: node mongo.js <password> [name] [number]');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = process.env.MONGO_URL.replace('<real_password>', password);
+
+mongoose.set('strictQuery',false);
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  // show all entries in the phonebook
+  Person.find({}).then(result => {
+    console.log('phonebook:');
+
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+
+    mongoose.connection.close();
+  });
+} else {
+  // add a new entry to the phonebook
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number
+  });
+
+  person.save().then(res => {
+    console.log(`added ${name} number ${number} to phonebook`);
+
+    mongoose.connection.close();
+  });
+}
