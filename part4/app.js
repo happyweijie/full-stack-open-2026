ba804d14d8ccb216f8dd8afe906e21d651d98ec0
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-16 22:35:32 +0800

    Refactor index.js into app.js

---
+++ part4/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,29 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-
-const app = express()
-
-mongoose.set('strictQuery', false)
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
