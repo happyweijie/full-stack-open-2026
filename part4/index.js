de457c78d5a140f4c3bf84fc283b18fbc48265fa
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-16 22:25:10 +0800

    Add model for blog

--- part4/index.js
+++ part4/index.js
@@ -1,28 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-
-mongoose.set('strictQuery', false)
+const Blog = require('./models/blog')
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+mongoose.set('strictQuery', false)
 mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
