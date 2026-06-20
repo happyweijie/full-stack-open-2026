46c12991c02ca11b594a6449f8d4f310aa6e9521
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-20 22:17:14 +0800

    Add totalLikes function

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -8,3 +8,56 @@ test('dummy returns one', () => {
   result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+
+  test('of list with 1 blog equals likes of that blog', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    ]
+
+    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
+  })
+
+  test('of larger list is calculated correctly', () => {
+    // 7 + 5 + 12 = 24
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      }
+    ]
+
+    assert.strictEqual(listHelper.totalLikes(blogs), 24)
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,12 @@
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  return blogs
+    .map(b => b.likes)
+    .reduce((x, y) => x + y, 0)
+}
+
 module.exports = { 
-  dummy
+  dummy,
+  totalLikes
 }
