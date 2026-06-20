bc11c2f584937edc8e16541c9bd7339fbc5a14c8
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-20 22:48:10 +0800

    Add method for finding blog with most likes

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -2,6 +2,45 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const LIST_WITH_ONE_BLOG = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    ]
+
+// likes: 7, 5, 12
+const BLOGS = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -15,49 +54,36 @@ describe('total likes', () => {
   })
 
   test('of list with 1 blog equals likes of that blog', () => {
-    const listWithOneBlog = [
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 5,
-        __v: 0
-      }
-    ]
-
-    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
+    assert.strictEqual(listHelper.totalLikes(LIST_WITH_ONE_BLOG), 5)
   })
 
   test('of larger list is calculated correctly', () => {
-    // 7 + 5 + 12 = 24
-    const blogs = [
-      {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-      },
-      {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-      },
-      {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-      }
-    ]
+    let total = 0
+    for (const b of BLOGS) {
+      total += b.likes
+    }
 
-    assert.strictEqual(listHelper.totalLikes(blogs), 24)
+    assert.strictEqual(listHelper.totalLikes(BLOGS), total)
+  })
+})
+
+describe('favourite blog of ', () => {
+  test('empty blogs list is undefined', () => {
+    assert.strictEqual(listHelper.favouriteBlog([]), undefined)
+  })
+
+  test('blog list with one blog is that blog', () => {
+    assert.deepStrictEqual(
+      listHelper.favouriteBlog(LIST_WITH_ONE_BLOG), 
+      LIST_WITH_ONE_BLOG.at(0)
+    )
+  })
+
+  test('blog list with multiple blogs is correct', () => {
+    // Blog at index 2 has 12 likes
+    assert.deepStrictEqual(
+      listHelper.favouriteBlog(BLOGS),
+      BLOGS[2]
+    )
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,22 @@ const totalLikes = (blogs) => {
     .reduce((x, y) => x + y, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  }
+
+  // Max likes
+  const mostLikes = Math.max(...blogs
+    .map(b => b.likes))
+
+  return blogs
+    .filter(b => b.likes === mostLikes)
+    .at(0);
+}
+
 module.exports = { 
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
