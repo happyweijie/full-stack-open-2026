b2220c84601f3a17223c904a944ad25869e6deb2
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-08-09 11:04:32 +0800

    Add users model

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
