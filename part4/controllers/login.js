c2b1d9e885dc07ff32d264fa97e95aa7679c4869
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-08-12 21:06:08 +0800

    Add login using token authentication

---
+++ part4/controllers/login.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const loginRouter = require('express').Router()
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user !== null
+    ? await bcrypt.compare(password, user.passwordHash)
+    : false
+
+  if (!user || !passwordCorrect) {
+    return response.status(401)
+      .status({ error: 'invalid username or password' })
+  }
+
+  const userForToken = { 
+    username: user.username,
+    id: user._id
+  }
+
+  const token = await jwt.sign(
+    userForToken, 
+    process.env.SECRET, 
+    { expiresIn: 60 * 60 }
+  )
+
+  response.status(200)
+    .send({ 
+      token,
+      username: user.username,
+      name: user.name
+    })
+})
+
+module.exports = loginRouter
