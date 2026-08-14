64260231aa8371348f6ca28c01a583ad7935ca4e
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-08-14 20:41:02 +0800

    Use user extractor middleware for adding new posts and deleting posts

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -10,24 +11,9 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
-
-  const decodedUserToken = jwt.verify(
-    request.token, 
-    process.env.SECRET)
-
-  if (!decodedUserToken) {
-    return response.status(401)
-      .json({ error: 'invalid token' })
-  }
-
-  // Get author of post from decoded token
-  const user = await User.findById(decodedUserToken.id)
-  if (!user) {
-    return response.status(400)
-      .json({ error: 'UserId missing or not valid' })
-  }
+  const user = request.user  
 
   if (!body.title || !body.url) {
     return response.status(400).json({
@@ -50,30 +36,15 @@ blogsRouter.post('/', async (request, response) => {
     .json(resultBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   // get blog
   const blog = await Blog.findById(request.params.id)
   if (!blog) {
     return response.status(204).end()
   }
 
-  // get request of user making the request
-  const decodedUserToken = jwt.verify(
-    request.token, 
-    process.env.SECRET)
-
-  if (!decodedUserToken) {
-    return response.status(401)
-      .json({ error: 'invalid token' })
-  }
-
-  // Get author of request from decoded token
-  const user = await User.findById(decodedUserToken.id)
-  if (!user) {
-    return response.status(400)
-      .json({ error: 'UserId missing or not valid' })
-  }
-
+  // get user making the request
+  const user = request.user
   if (blog.user.toString() !== user._id.toString()) {
     return response.status(401)
       .json({ error: 'only the creator can delete a blog' })
